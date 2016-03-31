625871d88ca76b34600ef03220d2ee85363ad32a
Surfin Bird <illvdg13@gmail.com>
2016-03-31 21:36:01 +0300

    missing part
    that was a big clean up

---
+++ AcManager.Tools/Helpers/Api/KunosApiProvider_Consts.cs
@@ -0,0 +1,8 @@
+﻿namespace AcManager.Tools.Helpers.Api {
+    public partial class KunosApiProvider {
+        private static readonly string[] ServerUris = { 
+            /* ADD YOUR API CODES HERE */
+        };
+        private static readonly string UserAgent = null; /* YOUR VALUE HERE */
+    }
+}
