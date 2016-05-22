f7ae226fe3556b11dec9befd9e573f7219bce420
Surfin Bird <illvdg13@gmail.com>
2016-05-22 17:44:33 +0300

    custom showroom (combining in progress)

---
+++ CustomShowroom/DebugHelper.cs
@@ -0,0 +1,62 @@
+namespace CustomShowroom {
+    public class DebugHelper {
+        public static string GetCarKn5() {
+            var kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_alfa_romeo_gta\alfaromeo_gta.kn5";
+            //kn5file = @"D:\Games\Steam\SteamApps\common\assettocorsa\content\cars\bmw_m3_e92\bmw_m3_e92.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ferrari_f2002\ferrari_f2002.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\F1ASR_ferrari_643\F1ASR_ferrari_643.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_lamborghini_gallardo_sl\lamborghini_gallardo_sl.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_lamborghini_huracan_st\lamborghini_huracan_st.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_alfa_mito_qv\alfa_romeo_mito_qv.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_abarth_595ss_s2\abarth_595ss.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_alfa_romeo_155_v6\Alfa_Romeo_155_V6.kn5";
+            //kn5file = @"D:\Games\Steam\SteamApps\common\assettocorsa\content\cars\ferrari_250gt_lusso\ferrari_250gt_lusso.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_audi_r8_plus\audi_r8_plus.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ruf_yellowbird\ruf_yellowbird.kn5";
+
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_ruf_rt12r\ruf_rt12r.kn5";
+            kn5file = @"D:\Games\Assetto Corsa\content\cars\reliant_robin\robin1.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_alfa_romeo_155_v6\collider.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_alfa_romeo_155_v6\Alfa_Romeo_155_V6.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_alfa_romeo_4c\alfa_romeo_4c.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ariel_atom_v8\ariel_atom_v8.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\peugeot_504\peugeot_504.kn5";
+            //kn5file = @"D:\Games\Steam\SteamApps\common\assettocorsa\content\cars\mtx_103c\mtx_103c.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_alfa_romeo_gta\alfaromeo_gta.kn5";
+
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_mercedes_c9\mercedes_c9.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_nissan_gtr\nissan_gtr.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\mercedes_sls\mercedes_sls.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_mclaren_p1\mclaren_p1.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_lamborghini_countach\lamborghini_countach.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_lotus_25\lotus_25.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_lotus_72d\lotus_72d.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\lotus_49\lotus_49.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\peugeot_504_tn\peugeot_504.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\acc_porsche_914-6\porsche_914-6.kn5";
+            //kn5file = @"D:\Games\Steam\SteamApps\common\assettocorsa\content\cars\vaz2106\vaz2106.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ktm_xbow_r\ktm_xbow_r.kn5"; // WARN
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_bmw_m4\bmw_m4.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\lotus_elise_sc\lotus_elise_sc.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\gmc_vandura\gmc_vandura.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_corvette_c7r\corvette_c7r.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_ford_escort_mk1\ford_escort_mk1.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_ford_mustang_2015\ford_mustang_2015.kn5";
+            //kn5file = @"D:\Development\GitHub\actools\AcManager\Assets\Data\ShowroomPreviewSphere\sphere.kn5";
+            //kn5file = @"D:\Games\Assetto Corsa\content\cars\ks_alfa_romeo_155_v6\Alfa_Romeo_155_V6_LOD_B.kn5";
+            //kn5file = @"D:\Games\Steam\SteamApps\common\assettocorsa\content\cars\__balls\sphere.kn5";
+
+            return kn5file;
+        }
+
+        public static string GetShowroomKn5() { 
+            var showroomKn5File = @"D:\Games\Assetto Corsa\content\showroom\showroom\showroom.kn5";
+            //showroomKn5File = @"D:\Games\Assetto Corsa\content\showroom\industrial\industrial.kn5";
+            //showroomKn5File = @"D:\Games\Assetto Corsa\content\showroom\hdri_2\hdri_2.kn5";
+            //showroomKn5File = @"D:\Games\Assetto Corsa\content\showroom\studio_smoke\studio_smoke.kn5";
+            //showroomKn5File = @"D:\Games\Assetto Corsa\content\showroom\greenfield\greenfield.kn5";
+
+            return showroomKn5File;
+        }
+    }
+}
---
+++ CustomShowroom/Program.cs
@@ -0,0 +1,241 @@
+﻿// ReSharper disable RedundantUsingDirective
+// #define PACKED_MODE
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Security.Permissions;
+using System.Windows.Forms;
+using AcTools.Render.Kn5SpecificDeferred;
+using AcTools.Render.Kn5SpecificForward;
+using AcTools.Render.Kn5SpecificSpecial;
+using AcTools.Render.Temporary;
+using AcTools.Render.Wrapper;
+using CommandLine;
+using CommandLine.Text;
+
+namespace CustomShowroom {
+    public enum Mode {
+        Custom, Lite, UpdateAmbientShadows, ExtractUv
+    }
+
+    public class Options {
+        [Option('m', "mode", DefaultValue =
+#if DEBUG
+            Mode.ExtractUv
+#else
+            Mode.Lite
+#endif
+            , HelpText = "App mode (Custom for fanciness or Lite for work).")]
+        public Mode Mode { get; set; }
+
+        [Option('a', "msaa", DefaultValue = false, HelpText = "Use MSAA (only for Lite Showroom mode).")]
+        public bool UseMsaa { get; set; }
+
+        [Option('x', "fxaa", DefaultValue = true, HelpText = "Use FXAA.")]
+        public bool UseFxaa { get; set; }
+
+        [Option('s', "showroom", DefaultValue = "showroom", HelpText = "Specific showroom ID (only for the Custom Showroom mode).")]
+        public string ShowroomId { get; set; }
+
+        [Option('t', "extract-texture", DefaultValue = null, HelpText = "Texture for which UV will be extracted.")]
+        public string ExtractUvTexture { get; set; }
+
+        [Option('v', "verbose", DefaultValue = false, HelpText = "Write some stuff to Log.txt near to exe-file.")]
+        public bool Verbose { get; set; }
+
+        [ValueList(typeof(List<string>), MaximumElements = 2)]
+        public IList<string> Items { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage() {
+            return HelpText.AutoBuild(this, current => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+
+    public class Program {
+        private static void UpdateAmbientShadows(string kn5) {
+            using (var renderer = new AmbientShadowKn5ObjectRenderer(kn5)) {
+                var dir = Path.GetDirectoryName(kn5);
+                renderer.Shot(dir);
+                Process.Start(Path.Combine(dir, "tyre_0_shadow.png"));
+            }
+        }
+
+        private static void ExtractUv(string kn5, string extractUvTexture) {
+            using (var renderer = new UvRenderer(kn5)) {
+                var dir = Path.GetDirectoryName(kn5);
+                var output = Path.Combine(dir, "extracted_uv.png");
+                renderer.Shot(output, extractUvTexture);
+                Process.Start(output);
+            }
+        }
+
+        [STAThread]
+        private static int Main(string[] a) {
+#if PACKED_MODE
+            Log(null);
+
+            var references = new ResourceManager("CustomShowroom.References", Assembly.GetExecutingAssembly());
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) => {
+                var name = args.Name.Split(new [] { "," }, StringSplitOptions.None)[0];
+                // Log(args.Name + " → " + name);
+                if (name == "CustomShowroom" || name.Contains(".resources")) return null;
+                Log(args.Name + " → " + name);
+
+                try {
+                    var bytes = references.GetObject(name) as byte[];
+                    if (bytes == null) {
+                        Log("missing!");
+                        return null;
+                    }
+
+                    Log("loaded: " + bytes.Length);
+                    return Assembly.Load(bytes);
+
+                    using (var memory = new MemoryStream(bytes))
+                    using (var output = new MemoryStream()) {
+                        using (var decomp = new DeflateStream(memory, CompressionMode.Decompress)) {
+                            decomp.CopyTo(output);
+                        }
+
+                        var res = output.ToArray();
+                        Log("loaded: " + res.Length);
+                        return Assembly.Load(res);
+                    }
+                } catch (Exception e) {
+                    Log("error: " + e);
+                    return null;
+                }
+            };
+#endif
+
+            return MainInner(a);
+        }
+
+        private static void InitializeLogging() {
+            var log = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) ?? "", "Log.txt");
+            try {
+                File.WriteAllBytes(log, new byte[0]);
+                Logging.Initialize(log);
+            } catch (Exception e) {
+                MessageBox.Show("Can't setup logging: " + e, @"Oops!", MessageBoxButtons.OK);
+            }
+        }
+        
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static int MainInner(string[] args) {
+            if (!Debugger.IsAttached) {
+                SetUnhandledExceptionHandler();
+            }
+
+            var options = new Options();
+            if (!Parser.Default.ParseArguments(args, options)) return 1;
+
+            var filename = Assembly.GetEntryAssembly().Location;
+            if (options.Verbose || filename.IndexOf("log", StringComparison.OrdinalIgnoreCase) != -1
+                    || filename.IndexOf("debug", StringComparison.OrdinalIgnoreCase) != -1) {
+                InitializeLogging();
+            }
+
+            var inputItems = options.Items;
+#if DEBUG
+            inputItems = inputItems.Any() ? inputItems : new[] { DebugHelper.GetCarKn5(), DebugHelper.GetShowroomKn5() };
+#endif
+
+            if (inputItems.Count == 0) {
+                var dialog = new OpenFileDialog {
+                    Title = @"Select KN5",
+                    Filter = @"KN5 Files (*.kn5)|*.kn5"
+                };
+                if (dialog.ShowDialog() != DialogResult.OK) return 2;
+
+                inputItems = new[] { dialog.FileName };
+            }
+
+            var kn5File = inputItems.ElementAtOrDefault(0);
+            if (kn5File == null || !File.Exists(kn5File)) {
+                MessageBox.Show(@"File is missing", @"Custom Showroom", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return 3;
+            }
+
+            if (options.Mode == Mode.UpdateAmbientShadows) {
+                UpdateAmbientShadows(kn5File);
+                return 0;
+            }
+
+            if (options.Mode == Mode.ExtractUv) {
+                if (string.IsNullOrWhiteSpace(options.ExtractUvTexture)) {
+                    MessageBox.Show(@"Texture to extract is not specified", @"Custom Showroom", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return 4;
+                }
+                ExtractUv(kn5File, options.ExtractUvTexture);
+                return 0;
+            }
+
+            var showroomKn5File = inputItems.ElementAtOrDefault(1);
+            if (showroomKn5File == null && options.ShowroomId != null) {
+                showroomKn5File = Path.Combine(
+                        Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(kn5File))) ?? "",
+                        "showroom", options.ShowroomId, options.ShowroomId + ".kn5");
+            }
+
+            if (!File.Exists(showroomKn5File)) {
+                showroomKn5File = null;
+            }
+            
+            if (options.Mode == Mode.Lite) {
+                using (var renderer = new ForwardKn5ObjectRenderer(kn5File)) {
+                    renderer.UseMsaa = options.UseMsaa;
+                    renderer.UseFxaa = options.UseFxaa;
+                    new LiteShowroomWrapper(renderer).Run();
+                }
+            } else {
+                using (var renderer = new Kn5ObjectRenderer(kn5File, showroomKn5File)) {
+                    renderer.UseFxaa = options.UseFxaa;
+                    new FancyShowroomWrapper(renderer).Run();
+                }
+            }
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            return 0;
+        }
+
+        [SecurityPermission(SecurityAction.Demand, Flags = SecurityPermissionFlag.ControlAppDomain)]
+        public static void SetUnhandledExceptionHandler() {
+            var currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += UnhandledExceptionHandler;
+        }
+
+        private static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs args) {
+            var e = args.ExceptionObject as Exception;
+
+            var text = "Unhandled exception:\n\n" + (e?.ToString() ?? "null");
+
+            try {
+                MessageBox.Show(text, @"Oops!", MessageBoxButtons.OK);
+            } catch (Exception) {
+                // ignored
+            }
+
+            try {
+                var logFilename = AppDomain.CurrentDomain.BaseDirectory + "/custom_showroom_crash_" + DateTime.Now.Ticks + ".txt";
+                File.WriteAllText(logFilename, text);
+            } catch (Exception) {
+                // ignored
+            }
+
+            Environment.Exit(1);
+        }
+    }
+}
