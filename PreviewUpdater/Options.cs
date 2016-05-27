f5d470305f44ff571102b8b914dfff30b56d1090
Surfin Bird <illvdg13@gmail.com>
2016-05-27 15:21:58 +0300

    Proper console PreviewUpdater

---
+++ PreviewUpdater/Options.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using CommandLine;
+using CommandLine.Text;
+
+namespace PreviewUpdater {
+    internal enum PseudoBool {
+        True = 1,
+        On = 1,
+        Yes = 1,
+        Y = 1,
+        False = 0,
+        Off = 0,
+        No = 0,
+        N = 0
+    }
+
+    internal class Options {
+        [ValueList(typeof(List<string>), MaximumElements = -1)]
+        public IList<string> Ids { get; set; }
+
+        [Option('r', "root", Required = true, HelpText = "AC root folder.")]
+        public string AcRoot { get; set; }
+
+        [Option('s', "showroom", DefaultValue = "previews", HelpText = "Showroom to shot previews at.")]
+        public string Showroom { get; set; }
+
+        [Option('c', "camera", DefaultValue = "-3.867643, 1.42359, 4.70381", HelpText = "Camera position.")]
+        public string CameraPosition { get; set; }
+
+        [Option('l', "look-at", DefaultValue = "0.0, 0.7, 0.5", HelpText = "Look at.")]
+        public string LookAt { get; set; }
+
+        [Option('v', "fov", DefaultValue = 30.0, HelpText = "Field of view.")]
+        public double Fov { get; set; }
+
+        [Option('e', "exposure", DefaultValue = 0.0, HelpText = "Exposure (looks like doesn't work).")]
+        public double Exposure { get; set; }
+
+        [Option('f', "filter", DefaultValue = "default", HelpText = "PP filter.")]
+        public string Filter { get; set; }
+
+        [Option('a', "fxaa", DefaultValue = PseudoBool.True, HelpText = "Enable FXAA.")]
+        public PseudoBool FxaaInner { get; set; }
+        public bool Fxaa => (int)FxaaInner == 1;
+
+        [Option('m', "maximize-settings", DefaultValue = PseudoBool.True, HelpText = "Maximize video settings.")]
+        public PseudoBool MaximizeVideoInner { get; set; }
+        public bool MaximizeVideo => (int)MaximizeVideoInner == 1;
+
+        [Option('4', "4k-resolution", DefaultValue = PseudoBool.True, HelpText = "Shot in 3840x2160 for the best quality.")]
+        public PseudoBool SpecialResolutionInner { get; set; }
+        public bool SpecialResolution => (int)SpecialResolutionInner == 1;
+
+        [Option('w', "without-previews-only", DefaultValue = false, HelpText = "Update skins only without previews.")]
+        public bool WithoutPreviews { get; set; }
+
+        [Option('t', "max-attempt", DefaultValue = 2, HelpText = "Max attempts per car if shooting is failed.")]
+        public int MaxAttempts { get; set; }
+
+        [Option('i', "ignore-errors", DefaultValue = false, HelpText = "Move to a next car if attempts number is exceeded.")]
+        public bool IgnoreErrors { get; set; }
+
+        [Option("filter-test", DefaultValue = false, HelpText = "Just list filtered cars and exit.")]
+        public bool FilterTest { get; set; }
+
+        [Option("verbose", HelpText = "Prints all messages to standard output.")]
+        public bool Verbose { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage() {
+            return HelpText.AutoBuild(this, current => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
---
+++ PreviewUpdater/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using AcTools.Processes;
+using AcTools.Utils;
+using AcTools.Utils.Helpers;
+using CommandLine;
+using StringBasedFilter;
+
+namespace PreviewUpdater {
+    internal class Program {
+        private static PackedHelper _helper;
+
+        [STAThread]
+        private static int Main(string[] a) {
+            _helper = new PackedHelper("AcTools_PreviewUpdater", "PreviewUpdater.References", false);
+            AppDomain.CurrentDomain.AssemblyResolve += _helper.Handler;
+            return MainInner(a);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static int MainInner(string[] args) {
+            var options = new Options();
+            if (!Parser.Default.ParseArguments(args, options) || options.Ids.Count == 0) return 1;
+
+            var acRoot = Path.GetFullPath(options.AcRoot);
+            var magick = _helper.GetFilename("Magick.NET-x86");
+            if (magick != null && File.Exists(magick)) {
+                try {
+                    ImageUtils.LoadImageMagickAssembly(magick);
+                } catch (Exception e) {
+                    Console.Error.WriteLine("Can't load ImageMagick assembly: " + e.Message);
+                }
+            }
+
+            IFilter<string> filter;
+
+            try {
+                filter = Filter.Create(new CarTester(acRoot), options.Ids.Select(x =>
+                        "(" + (x.EndsWith("/") ? x.Substring(0, x.Length - 1) : x) + ")").JoinToString("|"));
+                if (options.FilterTest) {
+                    Console.WriteLine(Directory.GetDirectories(FileUtils.GetCarsDirectory(options.AcRoot))
+                                               .Select(Path.GetFileName).Where(x => filter.Test(x)).JoinToString(", "));
+                    return 0;
+                }
+
+                if (options.Verbose) {
+                    Console.WriteLine("Filter: " + filter);
+                }
+            } catch (Exception e) {
+                Console.Error.WriteLine("Can't parse filter: " + e);
+                return 2;
+            }
+
+            if (options.Verbose) {
+                Console.WriteLine("ImageMagick: " + (ImageUtils.IsMagickSupported ? "Available" : "Not available"));
+            }
+
+            if (options.Verbose) {
+                Console.WriteLine("AC root: " + options.AcRoot);
+                Console.WriteLine("Showroom: " + options.Showroom);
+                Console.WriteLine("Filter: " + options.Filter);
+                Console.WriteLine("Camera position: " + options.CameraPosition);
+                Console.WriteLine("Look at: " + options.LookAt);
+                Console.WriteLine("FOV: " + options.Fov);
+                Console.WriteLine("Exposure: " + options.Exposure);
+                Console.WriteLine("FXAA: " + options.Fxaa);
+                Console.WriteLine("4K Resolution: " + options.SpecialResolution);
+                Console.WriteLine("Maximize video: " + options.MaximizeVideo);
+            }
+
+            if (options.Verbose) {
+                Console.WriteLine("Starting shoting...");
+            }
+
+            foreach (var id in Directory.GetDirectories(FileUtils.GetCarsDirectory(options.AcRoot))
+                                        .Select(Path.GetFileName).Where(x => filter.Test(x))) {
+                string[] skinIds = options.WithoutPreviews
+                        ? Directory.GetDirectories(FileUtils.GetCarSkinsDirectory(acRoot, id))
+                                   .Where(x => !File.Exists(Path.Combine(x, "preview.jpg")))
+                                   .Select(Path.GetFileName)
+                                   .ToArray()
+                        : null;
+
+                if (options.Verbose) {
+                    Console.WriteLine("Processing: " + id);
+
+                    if (options.WithoutPreviews) {
+                        Console.WriteLine("    Shot only: " + skinIds?.JoinToString(", "));
+                    }
+                }
+
+                int i;
+                for (i = 0; i < options.MaxAttempts; i++) {
+                    try {
+                        var shotted = Showroom.Shot(new Showroom.ShotProperties {
+                            AcRoot = acRoot,
+                            CarId = id,
+                            ShowroomId = options.Showroom,
+                            SkinIds = skinIds,
+                            Filter = options.Filter,
+                            Fxaa = options.Fxaa,
+                            SpecialResolution = options.SpecialResolution,
+                            MaximizeVideoSettings = options.MaximizeVideo,
+                            Mode = Showroom.ShotMode.Fixed,
+                            UseBmp = true,
+                            DisableWatermark = true,
+                            DisableSweetFx = true,
+                            ClassicCameraDx = 0.0,
+                            ClassicCameraDy = 0.0,
+                            ClassicCameraDistance = 5.5,
+                            FixedCameraPosition = options.CameraPosition,
+                            FixedCameraLookAt = options.LookAt,
+                            FixedCameraFov = options.Fov,
+                            FixedCameraExposure = options.Exposure,
+                        });
+
+                        if (shotted != null) {
+                            if (options.Verbose) {
+                                Console.WriteLine("    Applying previews from: " + shotted);
+                            }
+
+                            ImageUtils.ApplyPreviews(acRoot, id, shotted, true);
+                        } else {
+                            if (options.Verbose) {
+                                Console.WriteLine("    Nothing shotted");
+                            }
+                        }
+                        
+                        break;
+                    } catch (ShotingCancelledException e) {
+                        if (!e.UserCancelled) {
+                            Console.Error.WriteLine(e.Message);
+                        } else if (options.Verbose) {
+                            Console.Error.WriteLine("Cancelled");
+                        }
+
+                        return 3;
+                    } catch (Exception e) {
+                        Console.Error.WriteLine(e);
+                    }
+                }
+
+                if (i == options.MaxAttempts && !options.IgnoreErrors) {
+                    Console.Error.WriteLine("Attempts number exceeded, terminating");
+                    return 2;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
