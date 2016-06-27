146a91d089da1b4166c4ce79e7d91c924904416a
Surfin Bird <illvdg13@gmail.com>
2016-06-27 22:27:06 +0300

    A bit better, isn’t it?

--- FirstFloor.ModernUI/Windows/Controls/TagsList.cs
+++ FirstFloor.ModernUI/Windows/Controls/TagsList.cs
@@ -1,85 +1,37 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using FirstFloor.ModernUI.Presentation;
 using System.Collections.ObjectModel;
-using System.Collections.Specialized;
 using System.Windows.Data;
 using System.Windows.Input;
-using FirstFloor.ModernUI.Windows.Attached;
 
 namespace FirstFloor.ModernUI.Windows.Controls {
-    public class TagsListItem : Control {
-        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(string),
-            typeof(TagsListItem), new PropertyMetadata(OnValueChanged));
+    public class TagsList : Control {
+        private RelayCommand _closeCommand;
 
-        private static void OnValueChanged(DependencyObject o, DependencyPropertyChangedEventArgs e) {
-            ((TagsListItem)o).OnValueChanged((string)e.OldValue, (string)e.NewValue);
-        }
-
-        private void OnValueChanged(string oldValue, string newValue) {
-            if (_parent == null || oldValue == null) return;
-            if (string.IsNullOrWhiteSpace(newValue)) {
-                _parent.RemoveTag(oldValue);
-            } else {
-                _parent.ReplaceTag(oldValue, newValue.Trim());
-            }
-        }
-
-        public TagsListItem(string value, TagsList parent) {
-            DefaultStyleKey = typeof(TagsListItem);
-            Value = value;
-            _parent = parent;
-
-            SetValue(ContextMenuProperty, _parent.ItemContextMenu);
-            SetValue(ContextMenuAdvancement.PropagateToChildrenProperty, true);
-        }
-
-        public string Value {
-            get { return (string)GetValue(ValueProperty); }
-            set { SetValue(ValueProperty, value); }
-        }
-
-        private readonly TagsList _parent;
-
-        private ICommand _removeTagCommand;
-
-        public ICommand RemoveTagCommand => _removeTagCommand ?? (_removeTagCommand = new RelayCommand(o => {
-            if (_parent == null || Value == null) return;
-            _parent.RemoveTag(Value);
+        public RelayCommand CloseCommand => _closeCommand ?? (_closeCommand = new RelayCommand(o => {
+            ItemsSource.Remove(o as string);
         }));
-    }
 
-    public class TagsListCollection : ObservableCollection<TagsListItem> {
-        public TagsListCollection(IEnumerable<TagsListItem> list)
-            : base(list) {
-        }
-    }
+        private RelayCommand _changeCommand;
 
-    public class ReadOnlyTagsListCollection : ReadOnlyObservableCollection<TagsListItem> {
-        public ReadOnlyTagsListCollection(TagsListCollection list)
-            : base(list) {
-            List = list;
-        }
+        public RelayCommand ChangeCommand => _changeCommand ?? (_changeCommand = new RelayCommand(o => {
+            var target = (TextBox)o;
+            var originalValue = target.DataContext as string;
+            var newValue = target.Text.Trim();
+            if (string.IsNullOrEmpty(newValue)) {
+                ItemsSource.Remove(originalValue);
+            } else {
+                ItemsSource[ItemsSource.IndexOf(originalValue)] = target.Text;
+            }
+        }));
 
-        public ReadOnlyTagsListCollection(IEnumerable<TagsListItem> list)
-            : this(new TagsListCollection(list)) {
-        }
-
-        internal TagsListCollection List { get; private set; }
-    }
-
-    public partial class TagsList : Control {
         public static readonly DependencyProperty ItemsSourceProperty = DependencyProperty.Register("ItemsSource", typeof(ObservableCollection<string>),
-            typeof(TagsList), new PropertyMetadata(OnItemsSourceChanged));
+            typeof(TagsList));
 
         public static readonly DependencyProperty SuggestionsSourceProperty = DependencyProperty.Register("SuggestionsSource", typeof(CollectionView),
             typeof(TagsList));
-
-        private static readonly DependencyPropertyKey ItemsPropertyKey = DependencyProperty.RegisterReadOnly("Items", typeof(ReadOnlyTagsListCollection),
-            typeof(TagsList), null);
-        public static readonly DependencyProperty ItemsProperty = ItemsPropertyKey.DependencyProperty;
 
         public static readonly DependencyProperty ItemContextMenuProperty = DependencyProperty.Register(nameof(ItemContextMenu), typeof(ContextMenu),
                 typeof(TagsList));
@@ -102,34 +54,7 @@ namespace FirstFloor.ModernUI.Windows.Controls {
             get { return (CollectionView)GetValue(SuggestionsSourceProperty); }
             set { SetValue(SuggestionsSourceProperty, value); }
         }
-
-        private static void OnItemsSourceChanged(DependencyObject o, DependencyPropertyChangedEventArgs e) {
-            ((TagsList)o).OnItemsSourceChanged((ObservableCollection<string>)e.OldValue, (ObservableCollection<string>)e.NewValue);
-        }
-
-        private void OnItemsSourceChanged(ObservableCollection<string> oldValue, ObservableCollection<string> newValue) {
-            if (oldValue != null) {
-                oldValue.CollectionChanged -= ItemsSource_CollectionChanged;
-            }
-
-            UpdateReadOnlyCollection(newValue);
-
-            if (newValue != null) {
-                newValue.CollectionChanged += ItemsSource_CollectionChanged;
-            }
-        }
-
-        private void UpdateReadOnlyCollection(ObservableCollection<string> newValue) {
-            SetValue(ItemsPropertyKey, newValue == null ? null
-                    : new ReadOnlyTagsListCollection(from x in newValue select new TagsListItem(x, this)));
-        }
-
-        void ItemsSource_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e) {
-            UpdateReadOnlyCollection(sender as ObservableCollection<string>);
-        }
-
-        public ReadOnlyTagsListCollection Items => (ReadOnlyTagsListCollection)GetValue(ItemsProperty);
-
+        
         private ComboBox _previousTextBox;
 
         public override void OnApplyTemplate() {
@@ -141,7 +66,7 @@ namespace FirstFloor.ModernUI.Windows.Controls {
 
             base.OnApplyTemplate();
 
-            var textBox = Template.FindName("NewTagTextBox", this) as ComboBox;
+            var textBox = Template.FindName("PART_NewTagTextBox", this) as ComboBox;
             if (textBox == null) return;
 
             textBox.PreviewKeyDown += TextBox_KeyDown;
@@ -195,6 +120,16 @@ namespace FirstFloor.ModernUI.Windows.Controls {
         public bool IsReadOnly {
             get { return (bool)GetValue(IsReadOnlyProperty); }
             set { SetValue(IsReadOnlyProperty, value); }
+        }
+    }
+
+    public class TagsListDataTemplateSelector : DataTemplateSelector {
+        public DataTemplate TagDataTemplate { get; set; }
+
+        public DataTemplate NewTagDataTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container) {
+            return item is string ? TagDataTemplate : NewTagDataTemplate;
         }
     }
 }
