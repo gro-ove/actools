deb69d6465b224abb9766febb2308e780143ca7d
Surfin Bird <illvdg13@gmail.com>
2016-10-14 18:31:26 +0300

    0.7.648.12102

--- AcManager.Tools/Properties/AssemblyInfo.cs
+++ AcManager.Tools/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.139")]
-[assembly: AssemblyFileVersion("1.0.1.139")]
+[assembly: AssemblyVersion("1.0.1.140")]
+[assembly: AssemblyFileVersion("1.0.1.140")]
 
 [assembly: XmlnsDefinition("http://acstuff.ru/app/tools", "AcManager.Tools")]
 [assembly: XmlnsDefinition("http://acstuff.ru/app/tools", "AcManager.Tools.AcManagersNew")]
@@ -50,4 +50,4 @@ using System.Windows.Markup;
 [assembly: XmlnsPrefix("http://acstuff.ru/app/tools", "t")]
 
 [assembly: NeutralResourcesLanguage("en-US")]
-// Modified at: 10/11/2016 2:46:41 AM
+// Modified at: 10/14/2016 6:17:48 PM
