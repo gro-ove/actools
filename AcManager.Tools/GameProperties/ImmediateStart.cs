a0682f3bf7def7c9cbd97622dfd900339a9937bf
Surfin Bird <illvdg13@gmail.com>
2016-10-28 12:52:38 +0300

    Immediate Start feature now working with shared memory

--- AcManager.Tools/GameProperties/ImmediateStart.cs
+++ AcManager.Tools/GameProperties/ImmediateStart.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using AcManager.Tools.Helpers;
 using AcManager.Tools.Miscellaneous;
+using AcManager.Tools.SharedMemory;
 using AcTools.Processes;
 using AcTools.Windows.Input;
 
@@ -11,14 +12,30 @@ namespace AcManager.Tools.GameProperties {
         private bool _cancelled;
 
         public override IDisposable Set() {
-            Run().Forget();
+            if (SettingsHolder.Drive.WatchForSharedMemory) {
+                return SetSharedListener();
+            }
+
+            RunDelayed().Forget();
             return new ActionAsDisposable(() => _cancelled = true);
         }
 
-        private async Task Run() {
-            await Task.Delay(5000);
-            if (_cancelled) return;
+        public IDisposable SetSharedListener() {
+            EventHandler handler = null;
 
+            handler = (sender, args) => {
+                Run();
+                AcSharedMemory.Instance.GameStarted -= handler;
+            };
+
+            AcSharedMemory.Instance.GameStarted += handler;
+
+            return new ActionAsDisposable(() => {
+                AcSharedMemory.Instance.GameStarted -= handler;
+            });
+        }
+
+        public static void Run() {
             var originalPosition = Cursor.Position;
             var inputSimulator = new InputSimulator();
             inputSimulator.Mouse.MoveMouseTo(65536d * 50 / Screen.PrimaryScreen.Bounds.Width,
@@ -26,6 +43,13 @@ namespace AcManager.Tools.GameProperties {
             inputSimulator.Mouse.LeftButtonClick();
             inputSimulator.Mouse.MoveMouseTo(65536d * originalPosition.X / Screen.PrimaryScreen.Bounds.Width,
                     65536d * originalPosition.Y / Screen.PrimaryScreen.Bounds.Height);
+        }
+
+        private async Task RunDelayed() {
+            await Task.Delay(5000);
+            if (_cancelled) return;
+
+            Run();
         }
     }
 }
