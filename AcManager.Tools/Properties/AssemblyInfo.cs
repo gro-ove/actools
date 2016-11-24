d6536d466a98bcaac0ae585049de27aa600ffbe6
Surfin Bird <illvdg13@gmail.com>
2016-11-25 02:45:33 +0300

    Merging fixed

--- AcManager.Tools/Properties/AssemblyInfo.cs
+++ AcManager.Tools/Properties/AssemblyInfo.cs
@@ -34,13 +34,8 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-<<<<<<< HEAD
-[assembly: AssemblyVersion("1.0.1.460")]
-[assembly: AssemblyFileVersion("1.0.1.460")]
-=======
 [assembly: AssemblyVersion("1.0.1.481")]
 [assembly: AssemblyFileVersion("1.0.1.481")]
->>>>>>> master
 
 [assembly: XmlnsDefinition("http://acstuff.ru/app/tools", "AcManager.Tools")]
 [assembly: XmlnsDefinition("http://acstuff.ru/app/tools", "AcManager.Tools.AcManagersNew")]
