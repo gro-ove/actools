f05c6228aca1621ff9c1e31747bd1aee9f477022
DocWilco <github@drwil.co>
2021-04-07 11:59:13 +0200

    Support for hard lock with Fanatec DD2, needs more devices

---
+++ AcTools.WheelAngles/Implementations/Fanatec.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using HidLibrary;
+using JetBrains.Annotations;
+
+namespace AcTools.WheelAngles.Implementations {
+    [UsedImplicitly]
+    internal class Fanatec : IWheelSteerLockSetter {
+        public virtual string ControllerName => "Fanatec DD2";
+
+        public WheelOptionsBase GetOptions() {
+            return null;
+        }
+
+        public virtual bool Test(string productGuid) {
+            return string.Equals(productGuid, "00070EB7-0000-0000-0000-504944564944", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public int MaximumSteerLock => 65535;
+        public int MinimumSteerLock => 90;
+
+        private enum ScReportId : byte {
+            Out = 0x6B
+        }
+
+        private enum ScCommand : byte {
+            SetTemporaryVariable = 100
+        }
+
+        private enum ScValue1 : ushort {
+            TemporarySteeringAngle = 1,
+            UnsetTemporarySteeringAngle = 2
+        }
+        
+        public bool Apply(int steerLock, bool isReset, out int appliedValue) {
+            appliedValue = Math.Min(Math.Max(steerLock, MinimumSteerLock), MaximumSteerLock);
+
+            var cmd = new byte[] { 0x01, 0xf8, 0x81 }.Concat(BitConverter.GetBytes(appliedValue)).ToArray();
+
+            var result = HidDevices.Enumerate(0x0eb7, 0x0007).Aggregate(false, (a, b) => {
+                using (b) {
+                    AcToolsLogging.Write($"Set to {steerLock}: " + b.DevicePath);
+                    return a | b.Write(cmd);
+                }
+            });
+
+            return result;
+        }
+    }
+}
