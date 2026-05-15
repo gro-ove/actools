d57b47ea321437091b1b13a64a5efec29fbce627
Isaac Weisberg <net.caroline.weisberg@gmail.com>
2026-05-15 15:54:05 +0200

    Fix wrong default value

--- AcManager.Tools/Helpers/SettingsHolder.Drive.cs
+++ AcManager.Tools/Helpers/SettingsHolder.Drive.cs
@@ -753,7 +753,7 @@ namespace AcManager.Tools.Helpers {
             public string FilteredConnectedDrivers
             {
                 get => _filteredConnectedDrivers
-                        ?? (_filteredConnectedDrivers = ValuesStorage.Get("Settings.DriveSettings.FilteredConnectedDrivers", PlayerName));
+                        ?? (_filteredConnectedDrivers = ValuesStorage.Get("Settings.DriveSettings.FilteredConnectedDrivers"));
                 set
                 {
                     value = value?.Trim();
